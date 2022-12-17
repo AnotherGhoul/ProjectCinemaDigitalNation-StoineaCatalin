34dbbd842d0a8acb1612949a9d85d15dc0446e79
AnotherGhoul <63252473+AnotherGhoul@users.noreply.github.com>
2022-12-17 17:23:54 +0200

    Add files via upload

---
+++ StoineaCatalinEmployeesProject/src/main/java/in/stoineacatalin/springbootemplooyeapp/SpringBootEmployeeApp.java
@@ -0,0 +1,13 @@
+package in.stoineacatalin.springbootemplooyeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEmployeeApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEmployeeApp.class, args);
+	}
+
+}
